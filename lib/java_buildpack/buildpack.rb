0da79abd299bf86a7e4fa4ad08c510d9b561f4ba
Arun <shekhawataru@gmail.com>
2015-04-10 10:59:38 +0800

    no message

--- lib/java_buildpack/buildpack.rb
+++ lib/java_buildpack/buildpack.rb
@@ -81,6 +81,9 @@ module JavaBuildpack
       system 'chmod 600 .ssh/sshfs_rsa'
       system 'chmod 777 .ssh/known_hosts'
 
+      puts "++++++ Checking S3FS..."
+      system 's3fs'
+
       puts "++++++ Mounting Remote Directory..."
       system 'sshfs cpadmusr@160.153.90.232:sshfs /repos -o IdentityFile=.ssh/sshfs_rsa -odebug,sshfs_debug,loglevel=debug'
     
